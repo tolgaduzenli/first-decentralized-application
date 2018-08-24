47d23b367ef77853e09811c16ba5b5bff851cbcc
tolgaduzenli <4467TLG339tlg>
2018-08-24 14:51:37 +0200

    added client code and test code

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,25 @@
 pragma solidity ^0.4.24;
 
 contract Election{
-    string public candidate;
+    //Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Store candidates
+    mapping (uint => Candidate) public candidates;
+    //Store Candidates count
+    uint public candidatesCount;
+
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate ("Candidate 1");
+        addCandidate ("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
